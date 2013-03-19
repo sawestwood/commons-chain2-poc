d43b095889a8dc89f0d51525ab999b4e490ce32e
Steve Westwood <steve.westwood@informa.com>
2013-03-19 13:15:00 +0000

    Comment added

--- src/main/java/uk/org/hexsaw/OReillyOnJavaSampleAppPart1Page2.java
+++ src/main/java/uk/org/hexsaw/OReillyOnJavaSampleAppPart1Page2.java
@@ -6,6 +6,11 @@ import org.apache.commons.chain2.impl.ContextBase;
 import com.jadecove.chain.sample.SellVehicleChain;
 import com.jadecove.chain.sample.SellVehicleContext;
 
+/**
+ * This revisits example code demonstrating use of the <b>Apache Commons Commons-Chains</b> framework, first 
+ * published in  March 2005 on the O'Reilly OnJava site http://www.onjava.com/pub/a/onjava/2005/03/02/commonchains.html. 
+ * In this instance, the sample is implemented using commons-chain2.
+ */
 public class OReillyOnJavaSampleAppPart1Page2 {
 	
 	public static void main(String[] args) {
